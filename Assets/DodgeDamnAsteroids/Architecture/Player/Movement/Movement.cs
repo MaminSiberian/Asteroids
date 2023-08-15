fe34fb9ff80174d9c4b1540dab778b31226ac2e8
MaminSiberian <120947225+MaminSiberian@users.noreply.github.com>
2023-08-15 17:33:53 +0300

    Home

--- Assets/DodgeDamnAsteroids/Architecture/Player/Movement/Movement.cs
+++ Assets/DodgeDamnAsteroids/Architecture/Player/Movement/Movement.cs
@@ -9,6 +9,11 @@ public class Movement : MonoBehaviour
     private int speedMultiplier = 10;
     private Player player;
     private Rigidbody2D rb;
+    private float horizontalSpeed = InputManager.horizontalSpeed;
+    private float verticalSpeed = InputManager.verticalSpeed;
+    private float maxX;
+    private float maxY;
+    private float minY;
     private void OnEnable()
     {
         player = GetComponent<Player>();
@@ -17,6 +22,9 @@ public class Movement : MonoBehaviour
     private void Start()
     {
         GetSpeed();
+        this.maxX = player._maxX; 
+        this.maxY = player._maxY;
+        this.minY = player._minY;
     }
     private void GetSpeed()
     {
@@ -24,13 +32,32 @@ public class Movement : MonoBehaviour
     }
     private void Update()
     {
-        float rotDirection = InputManager.horizontalSpeed;
-        transform.rotation = Quaternion.Euler(0f, 0f, -rotDirection * rotationSpeed);
+        Rotate();
     }
     private void FixedUpdate()
     {
-        float horizontalSpeed = InputManager.horizontalSpeed;
-        float verticalSpeed = InputManager.verticalSpeed;
+        Move();
+    }
+    private void Move()
+    {
+        horizontalSpeed = InputManager.horizontalSpeed;
+        verticalSpeed = InputManager.verticalSpeed;
+
+        CheckPosition();
+
         rb.velocity = new Vector2(horizontalSpeed * moveSpeed * Time.fixedDeltaTime, verticalSpeed * moveSpeed * Time.fixedDeltaTime);
+    }
+    private void CheckPosition()
+    {
+        if (this.transform.position.x <= -maxX && horizontalSpeed < 0 || this.transform.position.x >= maxX && horizontalSpeed > 0)
+            horizontalSpeed = 0;
+
+        if (this.transform.position.y <= minY && verticalSpeed < 0 || this.transform.position.y >= maxY && verticalSpeed > 0)
+            verticalSpeed = 0;
+    }
+    private void Rotate()
+    {
+        float rotDirection = InputManager.horizontalSpeed;
+        transform.rotation = Quaternion.Euler(0f, 0f, -rotDirection * rotationSpeed);
     }
 }
--- Assets/DodgeDamnAsteroids/Architecture/Player/Player.cs
+++ Assets/DodgeDamnAsteroids/Architecture/Player/Player.cs
@@ -8,11 +8,22 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed;
     public float _moveSpeed { get; private set; }
     [SerializeField] private float maxX;
-    public int _maxX { get; private set; }
+    public float _maxX { get; private set; }
+    [SerializeField] private float minY;
+    public float _minY { get; private set; }
+    [SerializeField] private float maxY;
+    public float _maxY { get; private set; }
 
     private void Awake()
     {
         this.gameObject.AddComponent<Movement>();
         this._moveSpeed = this.moveSpeed;
+        this._maxX = this.maxX;
+        this._maxY = this.maxY;
+        this._minY = this.minY;
+    }
+    public void OnPlayerDeath()
+    {
+
     }
 }
